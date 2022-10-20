c0fbad6dd3407ca558edf1779a32644b2f6cec5f
Saladikhanov <saladikhanov@gmail.com>
2022-10-20 17:05:09 +0300

    final

---
+++ imagga-client/src/main/java/telran/imagga/controller/ImaggaTagController.java
@@ -0,0 +1,33 @@
+package telran.imagga.controller;
+
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import telran.imagga.dto.ResponseTagDto;
+
+public class ImaggaTagController {
+    static RestTemplate restTemplate = new RestTemplate();
+    static final String TOKEN = "Basic YWNjXzNjZDkzMTA4ZTdmMjEyZjozYzY0ZDgzNmFiN2RjNTVhZTFkZGY1YzRiZGZiMmY4MA==";
+    static String img = "https://24smi.org/public/media/235x307/person/2017/12/22/4sqqykgn04bo-cheburashka.jpg";
+    static String baseUrl = "https://api.imagga.com/v2/tags";
+
+    public static void main(String[] args) {
+	HttpHeaders headers = new HttpHeaders();
+	headers.add(HttpHeaders.AUTHORIZATION, TOKEN);
+	UriComponentsBuilder builder =  UriComponentsBuilder.fromHttpUrl(baseUrl)
+																.queryParam("image_url", img)
+																.queryParam("language", "uk")
+																.queryParam("limit", 3);
+	RequestEntity<String> requestEntity = new RequestEntity<>(headers , HttpMethod.GET, builder.build().toUri());
+	ResponseEntity<ResponseTagDto> responseEntity = restTemplate.exchange(requestEntity, ResponseTagDto.class);
+	responseEntity.getBody().getResult().getTags().forEach(System.out::println);
+    }
+
+}
